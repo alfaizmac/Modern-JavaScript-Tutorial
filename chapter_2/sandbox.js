9fa982324c0710afe9685a6b2fd17cc0a1c7024b
Alfaiz Mac <macalangcomalfaiz2@gmail.com>
2025-01-18 12:15:38 +0800

    next

---
+++ chapter_2/sandbox.js
@@ -0,0 +1,53 @@
+// // alert("hello word");
+
+// // console.log(2);
+// // console.log(1);
+
+// let age = 25;
+// let year = 2025;
+// console.log(age, year);
+
+// age = 30;
+// console.log(age);
+
+// //const will remain all the time unlike Let.
+// const points = 100;
+// // points = 50;
+// console.log(points);
+
+// var score = 75;
+// console.log(score);
+
+// score = 20;
+// console.log(score);
+
+//strings
+console.log("hello, world");
+
+let email = "mario@gmail.com";
+console.log(email);
+
+// string concatenation
+
+let fName = "Mario";
+let lName = "Bros";
+
+let fullName = fName + " " + lName;
+console.log(fullName);
+
+// getting characters
+console.log(fullName[1]);
+
+// stirng lenghth
+console.log(fullName.length);
+
+// string methods
+console.log(fullName.toUpperCase());
+
+let result = fullName.toLowerCase();
+
+console.log(result, fullName);
+
+let index = email.indexOf("@");
+
+console.log(index);
